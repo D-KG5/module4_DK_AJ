1c3499c5e8fe68aa072e0b1ba62d7917d0ae5ae0
Dhruva Koley <dhruvakoley@gmail.com>
2020-10-24 20:39:38 -0600

    cleanup of code

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #define ADC_12BIT 4096.0
 #define ADC_VREF 3.30
 /* FFT settings */
-#define SAMPLES                    512             /* 256 real party and 256 imaginary parts */
+#define SAMPLES                    2048             /* 256 real party and 256 imaginary parts */
 #define FFT_SIZE                SAMPLES / 2        /* FFT size is always the same size as we have samples, so 256 in our case */
 
 float32_t Input[SAMPLES];
@@ -23,7 +23,7 @@ void fake_dac(void){
         voltage = 2 + arm_sin_f32((float32_t)(2008.0 * PI * step)); // 1004 Hz sine wave
         fake_dac_buffer[i] = (float32_t) ((ADC_12BIT * voltage / ADC_VREF) - 1);
         step += 0.00002;
-        printf("%f\r", voltage);
+        //printf("%f\r", fake_dac_buffer[i]);
     }
 }
 
@@ -32,7 +32,6 @@ int main()
 {
     TM_FFT_F32_t FFT;
     uint16_t i = 0;
-    uint32_t frequency = 10000;
 
     TM_FFT_Init_F32(&FFT, FFT_SIZE, 0);
 
@@ -44,14 +43,10 @@ int main()
         //while loop to add samples at fixed rate
         while(!TM_FFT_AddToBuffer(&FFT, (fake_dac_buffer[i] - (float32_t)2048.0))){
             i++;
-            // if(i == (FFT_SIZE - 1)){
-            //     break;
-            // }
             wait_us(23); // sample rate faking
         }
 
         TM_FFT_Process_F32(&FFT);
-        wait_us(1000000);
 
         // print fundamental freq
         printf("Fundamental frequency is %d Hz\r\n", TM_FFT_GetMaxIndex(&FFT));
